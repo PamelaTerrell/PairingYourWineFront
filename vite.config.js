a82ef515f034fc892d6fd4ac5c5fa25588a0f02d
PamelaTerrell <pammyhoney@yahoo.com>
2024-11-22 10:21:45 -0500

    prepForDeploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'http://vinopairings.com',
   plugins: [react()],
+  build: {
+    outDir: 'dist',   // Explicitly set the output directory to 'dist'
+    assetsDir: 'assets', // Specify the assets directory inside 'dist'
+    rollupOptions: {
+      input: 'index.html', // Ensure the correct entry point is used
+    },
+  },
 })
+

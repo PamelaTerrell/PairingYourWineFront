7ed16bfa1988afb29198daa3153acbc496a62004
PamelaTerrell <pammyhoney@yahoo.com>
2024-11-22 10:28:24 -0500

    deployPrep

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
       
       <p>
         You probably know the golden rule of thumb about wine pairings: Red wines pair best with bold-flavored proteins (red meat) and white wines pair best with light-intensity proteins (chicken or fish). 
-        Of course, there's more to making successful wine-and-food pairings. The key to finding the perfect match is to balance the flavors, textures, and aromas of both the food and the wine.
+        Of course, there's more to making successful wine-and-food pairings. The key to finding the perfect match is to balance the flavors, textures, and aromas of both the food and the vino.
       </p>
       <p>
         Wine pairing isn't just about rules—it's about enhancing your overall dining experience. For example, a rich, full-bodied red like Cabernet Sauvignon can bring out the best in a steak, while a crisp Sauvignon Blanc might complement the subtle flavors of a seafood dish. 
